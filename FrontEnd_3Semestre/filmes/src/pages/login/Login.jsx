e603df36e60874105500dd957f112877883dd4f5
FRANCISCO HUGO XIMENES SALES <44788197855@info.local>
2026-06-02 16:20:38 -0300

    Feito a migração da tela login

--- FrontEnd_3Semestre/filmes/src/pages/login/Login.jsx
+++ FrontEnd_3Semestre/filmes/src/pages/login/Login.jsx
@@ -1,9 +1,45 @@
-import "./login.css"
+import Logo from "../../assets/img/logo.svg";
+import "./Login.css";
+import Botao from "../../components/botao/Botao";
 
-const Login = () =>{
-    return(
-        <h1>pagina de login </h1>
-    )
-}
+const Login = () => {
+    return (
+        <main className="main_login">
+            <div className="banner"></div>
 
-export default Login
+            <section className="section_login">
+                <img src={Logo} alt="Logo do Filmoteca" />
+
+                <form className="form_login">
+                    <h1>Login</h1>
+
+                    <div className="campos_login">
+                        <div className="campo_input">
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                placeholder="Digite seu e-mail"
+                            />
+                        </div>
+
+                        <div className="campo_input">
+                            <label htmlFor="senha">Senha:</label>
+                            <input
+                                type="password"
+                                id="senha"
+                                name="senha"
+                                placeholder="Digite sua senha"
+                            />
+                        </div>
+                    </div>
+
+                    <Botao nomeDoBotao="Entrar" />
+                </form>
+            </section>
+        </main>
+    );
+};
+
+export default Login;
